b8579a543048a1bf4f7be5c0dabcd81fc018e512
hamidaucc <abdulhamid000@yahoo.com>
2016-09-28 21:46:44 +0100

    Update EmptyError.py

--- EmptyError.py
+++ EmptyError.py
@@ -2,9 +2,9 @@
 __module__="EmptyError"
 import StackError as error
 class EmptyError(error.StackError):
-    # @author Herakliusz Lipiec (ID 114345041)
-    # @author Hamid Abdul (ID 114734769)
-    # @author Stephen Hannon(ID 113425638)
+    
+    # @author Hamid Abdul 
+ 
     
     # Error class that is raised when trying 
     # to access empty stack through indexing
